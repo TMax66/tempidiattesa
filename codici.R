e16ada62f2f6626550815a1f8e8c5150cf8beaef
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-03-28 14:58:18 +0200

    update

--- codici.R
+++ codici.R
@@ -23,18 +23,53 @@ saveRDS(taut, file = "taut.RDS")
 dt <- readRDS("taut.RDS")
 rm(taut)
 
+##questo codice permette di fleggare il conferimento come conferimento con prove inviate ad altri lab o conferimento
+ ## con prove eseguite tutte nel laboratorio di conferimento del campione, nel primo caso è sufficiente che ci sia una sola prova eseguita
+  ## in un laboratorio differente da quello che ha ricevuto il conferimento dal proprietario per essere fleggato come confeirmento con altri lab
 
+
+altrilab <- dt %>% 
+  mutate(daescl = is.na(dt$Data_RDP), 
+         altrilab = ifelse(is.na(Data_Invio), 0, 1)) %>%  
+  group_by(nconf) %>% 
+  summarise(altrilab = sum(altrilab, na.rm = TRUE)) %>% 
+  filter(altrilab>=1) %>% 
+  distinct(nconf) %>% 
+  mutate(Altrilab = "conferimento con altri lab")  
 
 
 dt %>% 
-  # mutate(daescl = is.na(dt$DataOra_Primo_RDP_Completo_Firmato)) %>% 
-  distinct(nconf, .keep_all = TRUE) %>%
+  left_join(altrilab, by="nconf") %>%  
+  filter(is.na(Altrilab)) %>%   
+  # mutate(daescl = is.na(dt$Data_RDP)) %>%  View()
+  # filter(daescl == FALSE) %>%  View()
+  mutate(taut = (Data_RDP-Data)/86400) %>%  
+  group_by(nconf,   Finalita, taut) %>% 
+  summarise(Esami = sum(Tot_Eseguiti, na.rm = TRUE)) %>% 
   group_by(Finalita) %>% 
-  summarise(n = n(), 
-            esami = sum(Tot_Eseguiti, na.rm = TRUE), 
-            taut = mean((Data_RDP-Data)/86400))  %>%  View()
+  summarise(n=n(), 
+            Esami = sum(Esami), 
+            TAUT = quantile(taut,probs = 0.50, na.rm= TRUE)) %>%  View()
+  
+  
+
+
+  
+
+
+
+
+  
+  
+
+
+# summarise(n = n(), 
+  #         
+  #           taut = (Data_RDP-Data)/86400)  %>%  
   #count() %>% 
   #filter(daescl == FALSE) %>% 
+    
+  distinct(nconf, .keep_all = TRUE) %>%
   arrange(desc(n)) %>% View()
 
 dt %>% 
