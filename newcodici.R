7b5f4340fd87ec59e8473363ff73f7df83ff4d16
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2023-06-02 18:01:56 +0200

    update

--- newcodici.R
+++ newcodici.R
@@ -20,11 +20,12 @@ con <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "dbprod02",
 source("sql.R")
 
 
-taut <- con %>% tbl(sql(queryTaut)) %>% as_tibble() 
-#saveRDS(taut, file = "taut.RDS")
+taut <- con %>% tbl(sql(queryTaut)) %>% as_tibble()
+
+
+saveRDS(taut, file = "taut.RDS")
 
 dati <- readRDS(file = "taut.RDS" )
-
 
 
 
@@ -33,36 +34,188 @@ dati <- readRDS(file = "taut.RDS" )
 # questo codice calcola per ogni prova i tempi di firma, di risposta e il tempo di attesa e considera se la prova
 # è eseguita  nella struttura accettante o se è eseguita in altra struttura....
 
-tempi <- function(dati, prova)
-{
-  dati %>% 
-    filter(prova == prova) %>% 
-    mutate(
-      in_out = case_when(
-        stracc != repprova ~ paste("out"), 
-        TRUE ~ paste("in")), 
-      
-      taut = (Data_RDP-Data)/86400,
-      
-      trisp = case_when(
-        in_out == "in" ~ (Data_Fine_Analisi-Data_Accettazione)/86400,
-        TRUE~  ((Data_Fine_Analisi- Data_Carico)/86400)
-      ), 
-      tfirma = (Data_RDP-Data_Fine_Analisi), 
-      
-      torg =  case_when(
-        in_out == "in" ~ (Data_Inizio_Analisi-Data_Accettazione)/86400,
-        TRUE~  ((Data_Inizio_Analisi- Data_Carico)/86400)
-      )
-      
-      
-    ) %>% 
-    filter(tfirma >= 0)
-  
-  
-}
+# tempi <- function(dati, prova)
+# {
+#   dati %>% 
+#     filter(prova == prova) %>% 
+#     mutate(
+#       in_out = case_when(
+#         stracc != repprova ~ paste("out"), 
+#         TRUE ~ paste("in")), 
+#       taut = (Data_RDP-Data)/86400,
+#       trisp = case_when(
+#         in_out == "in" ~ (Data_Fine_Analisi-Data_Accettazione)/86400,
+#         TRUE~  ((Data_Fine_Analisi- Data_Carico)/86400)
+#       ), 
+#       tfirma = (Data_RDP-Data_Fine_Analisi), 
+#       torg =  case_when(
+#         in_out == "in" ~ (Data_Inizio_Analisi-Data_Accettazione)/86400,
+#         TRUE~  ((Data_Inizio_Analisi- Data_Carico)/86400)
+#       )
+#     ) %>% 
+#     filter(tfirma >= 0)
+# }
 
-tempi(dati, prova = "Grasso") %>% 
+
+
+dati <- dati %>%
+  mutate(dtprel = as.Date(strftime(Data_Prelievo,format = "%Y-%m-%d")),
+         dtconf = as.Date(strftime(dtconf, format = "%Y-%m-%d")),
+         dtreg = as.Date(strftime(dtreg, format = "%Y-%m-%d" )),
+         dtprog = as.Date(strftime(dtprog, format = "%Y-%m-%d" )),
+         dtinvio = as.Date(strftime(Data_Invio, format = "%Y-%m-%d")),
+         dtcarico = as.Date(strftime(Data_Carico, format = "%Y-%m-%d")),
+         dtinizio = as.Date(strftime(Data_Inizio_Analisi, format = "%Y-%m-%d")),
+         dtfine = as.Date(strftime(Data_Fine_Analisi, format = "%Y-%m-%d")),
+         dt1rdp = as.Date(strftime(Data_Primo_RDP_Completo_Firmato, format = "%Y-%m-%d")))
+
+
+dt <- dati %>% 
+  select(numero = Numero, 
+         finalita,
+         dtprel = Data_Prelievo,
+         dtconf,
+         dtreg,
+         dtprog,
+         dtinvio, 
+         dtcarico, 
+         dtinizio,
+         dtfine,  
+         dt1rdp,  
+         stracc = repacc,  
+         repprop,
+         repanalisi, 
+         labanalisi, 
+         categprove, 
+         gruppo,
+         prova,
+         mp) %>% 
+#filter(prova == "BHV1/Rinotracheite Infettiva Bovina:anticorpi verso gE del virus") %>% 
+  mutate(
+    in_out = case_when(
+      stracc != repanalisi ~ paste("out"), 
+      TRUE ~ paste("in"))) %>% 
+    mutate( 
+      trisp = case_when(
+        in_out == "in" ~ (dtfine-dtreg),
+        TRUE~  ((dtfine - dtcarico))
+      ), 
+      stracc2= abbreviate(stracc))  
+
+saveRDS(dt, "dt.RDS")
+
+
+network <- dt %>% 
+  group_by(stracc, repanalisi) %>% 
+  count() %>% 
+  pivot_wider(names_from = "repanalisi", values_from = "n", values_fill = 0) %>% 
+  #filter(finalita == "Controllo alimenti") %>% 
+  ungroup() %>%  #select(stracc, `Sede Territoriale di Bergamo`) %>% 
+  #select(- finalita) %>% 
+  filter(stracc == "Sede Territoriale di Sondrio") %>% 
+  column_to_rownames("stracc") 
+
+
+ 
+
+network <- dt %>% 
+  #filter(stracc == "Sede Territoriale di Cremona") %>% 
+  #filter(finalita == "Esame trichinoscopico") %>% 
+  filter(repanalisi == "Sede Territoriale di Cremona") %>% 
+  #group_by(repanalisi, stracc) %>% 
+  group_by(stracc, repanalisi) %>% 
+  count() 
+
+net <- graph_from_data_frame(d=network, directed=T) 
+
+plot(net, edge.arrow.size=0.1, edge.width=E(net)$n/1000)
+
+plot(simplify(net), vertex.size= 0.01,edge.arrow.size=0.001,vertex.label.cex = 0.75,
+     vertex.label.color = "black"  ,vertex.frame.color = adjustcolor("white", alpha.f = 0),
+     vertex.color = adjustcolor("white", alpha.f = 0),edge.color=adjustcolor(1, alpha.f = 0.15),
+     display.isolates=FALSE,vertex.label=ifelse(page_rank(net)$vector > 0.1 , "important nodes", NA))
+
+
+# library(igraph)
+# 
+# net <- graph_from_incidence_matrix(network)
+
+
+
+
+
+# adj_mat <- as_adjacency_matrix(
+#   graph_from_data_frame(network, 
+#                         directed = TRUE))
+# 
+# 
+# 
+# graph_obj <- graph.adjacency(adj_mat)
+# 
+# plot.igraph(graph_obj,
+#             layout=layout.star(graph_obj),
+#             vertex.color=NA, vertex.frame.color = NA
+# )
+# 
+# net <-  graph.data.frame(network, directed=T)
+
+#net <- simplify(net)
+
+
+#net <- graph_from_incidence_matrix(network)
+
+
+
+  #filter(trisp >= 0) %>% 
+  # group_by(Finalita, prova, in_out, stracc, repprova) %>% 
+  # summarise(n = n(),
+  #   trisp = mean(trisp, na.rm = TRUE)) %>%  
+  
+  #group_by(stracc, in_out) %>% 
+  # summarise(n = n(),
+  #           trisp = mean(trisp, na.rm = TRUE)) %>%  
+ggplot()+
+  aes(x = stracc2, y = trisp)+
+  geom_violin()+
+  #geom_jitter(alpha = 0.3)+
+  stat_summary(fun = "mean", color = "red")+
+  facet_wrap(~ in_out, nrow = 1, scales = "free")+
+  theme_classic()+
+  coord_flip()+
+  theme(
+    panel.grid.major = element_line()
+  )+
+  
+  labs(x= "Anno", y= "IF", title = "")
+  
+  
+    
+
+
+
+#taut = (Data_RDP-Data)/86400) %>%  View()
+    
+  #   
+  #   
+  #   
+  # 
+  #   trisp = case_when(
+  #     in_out == "in" ~ (Data_Fine_Analisi-Data_Accettazione)/86400,
+  #     TRUE~  ((Data_Fine_Analisi- Data_Carico)/86400)
+  #   ), 
+  #   tfirma = (Data_RDP-Data_Fine_Analisi), 
+  #   torg =  case_when(
+  #     in_out == "in" ~ (Data_Inizio_Analisi-Data_Accettazione)/86400,
+  #     TRUE~  ((Data_Inizio_Analisi- Data_Carico)/86400)
+  #   )
+  # ) %>% 
+  # filter(tfirma >= 0)  
+  
+
+
+x <- dati %>% filter(nconf == 88)
+
+tempi(x, prova = "Grasso") %>% 
   group_by(in_out) %>% 
   summarise(Mtaut = mean(taut), 
             Mtrisp = mean(trisp, na.rm = TRUE), 
--- sql.R
+++ sql.R
@@ -1,70 +1,98 @@
-queryTaut <- "SELECT
-  dbo.Conferimenti.Data_Prelievo,
-  dbo.Conferimenti.Data,
-  dbo_Anag_Reparti_ConfAcc.Descrizione As stracc,
-  dbo_Anag_Reparti_ConfProp.Descrizione As strapp,
-  dbo.Conferimenti.Numero As nconf,
-  dbo_Anag_Finalita_Confer.Descrizione As Finalita,
-  Datename(weekday, dbo.Conferimenti.Data_Accettazione) As giorno,
-  dbo.Conferimenti.Data_Accettazione,
-  dbo.Esami_Aggregati.Codice As codprogram,
-  dbo.Anag_Prove.Descrizione As prova,
-  dbo.Esami_Aggregati.Data_Invio,
-  dbo.Esami_Aggregati.Data_Carico,
-  dbo.Anag_Reparti.Descrizione As repprova,
-  dbo.Anag_Laboratori.Descrizione As Laboratorio,
-  dbo.Esami_Aggregati.Data_Inizio_Analisi,
-  dbo.Esami_Aggregati.Data_Fine_Analisi,
-  dbo.RDP_Date_Emissione.Data_RDP,
-  dbo.Conferimenti.DataOra_Primo_RDP_Completo_Firmato,
-  dbo.Conferimenti.NrCampioni,
-  dbo.Esami_Aggregati.Tot_Eseguiti,
-  dbo.Nomenclatore.Chiave,
-  dbo.Anag_Metodi_di_Prova_Base.Descrizione As mmpp,
-  dbo.Anag_Gruppo_Prove.Descrizione As catprova,
-  dbo.Anag_Registri.Descrizione As reg,
-  dbo.Anag_Tipo_Prel.Descrizione As tipoprelievo,
-  dbo.Anag_Sottotipo_Prel.Descrizione As sottotipoprel,
-  dbo.Anag_Sottotipo_Prel_Dett.Descrizione As stipopreldesc,
-  dbo.Anag_Comuni.Descrizione As Comune ,
-  dbo.Anag_Comuni.Provincia,
-  dbo.Anag_Regioni.Descrizione As Regione
-FROM
-{ oj dbo.Anag_Reparti  dbo_Anag_Reparti_ConfAcc INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfAcc ON ( dbo_Laboratori_Reparto_ConfAcc.Reparto=dbo_Anag_Reparti_ConfAcc.Codice )
-   INNER JOIN dbo.Conferimenti ON ( dbo.Conferimenti.RepLab_Conferente=dbo_Laboratori_Reparto_ConfAcc.Chiave )
-   INNER JOIN dbo.Anag_Comuni ON ( dbo.Anag_Comuni.Codice=dbo.Conferimenti.Luogo_Prelievo )
-   LEFT OUTER JOIN dbo.Anag_Regioni ON ( dbo.Anag_Regioni.Codice=dbo.Anag_Comuni.Regione )
-   LEFT OUTER JOIN dbo.Esami_Aggregati ON ( dbo.Conferimenti.Anno=dbo.Esami_Aggregati.Anno_Conferimento and dbo.Conferimenti.Numero=dbo.Esami_Aggregati.Numero_Conferimento )
-   LEFT OUTER JOIN dbo.Nomenclatore_MP ON ( dbo.Esami_Aggregati.Nomenclatore=dbo.Nomenclatore_MP.Codice )
-   LEFT OUTER JOIN dbo.Nomenclatore_Settori ON ( dbo.Nomenclatore_MP.Nomenclatore_Settore=dbo.Nomenclatore_Settori.Codice )
-   LEFT OUTER JOIN dbo.Nomenclatore ON ( dbo.Nomenclatore_Settori.Codice_Nomenclatore=dbo.Nomenclatore.Chiave )
-   LEFT OUTER JOIN dbo.Anag_Prove ON ( dbo.Nomenclatore.Codice_Prova=dbo.Anag_Prove.Codice )
-   LEFT OUTER JOIN dbo.Anag_Gruppo_Prove ON ( dbo.Nomenclatore.Codice_Gruppo=dbo.Anag_Gruppo_Prove.Codice )
-   LEFT OUTER JOIN dbo.Anag_Metodi_di_Prova_Revisioni ON ( dbo.Nomenclatore_MP.MP=dbo.Anag_Metodi_di_Prova_Revisioni.Codice )
-   LEFT OUTER JOIN dbo.Anag_Metodi_di_Prova_Base ON ( dbo.Anag_Metodi_di_Prova_Revisioni.MP_Base=dbo.Anag_Metodi_di_Prova_Base.Codice )
-   LEFT OUTER JOIN dbo.Laboratori_Reparto ON ( dbo.Esami_Aggregati.RepLab_analisi=dbo.Laboratori_Reparto.Chiave )
-   LEFT OUTER JOIN dbo.Anag_Reparti ON ( dbo.Laboratori_Reparto.Reparto=dbo.Anag_Reparti.Codice )
-   LEFT OUTER JOIN dbo.Anag_Laboratori ON ( dbo.Laboratori_Reparto.Laboratorio=dbo.Anag_Laboratori.Codice )
-   INNER JOIN dbo.Anag_Tipo_Prel ON ( dbo.Conferimenti.Tipo_Prelievo=dbo.Anag_Tipo_Prel.Codice )
-   INNER JOIN dbo.Anag_Registri ON ( dbo.Conferimenti.Registro=dbo.Anag_Registri.Codice )
-   INNER JOIN dbo.Laboratori_Reparto  dbo_Laboratori_Reparto_ConfProp ON ( dbo.Conferimenti.RepLab=dbo_Laboratori_Reparto_ConfProp.Chiave )
-   INNER JOIN dbo.Anag_Reparti  dbo_Anag_Reparti_ConfProp ON ( dbo_Laboratori_Reparto_ConfProp.Reparto=dbo_Anag_Reparti_ConfProp.Codice )
-   LEFT OUTER JOIN dbo.Anag_Sottotipo_Prel ON ( dbo.Anag_Sottotipo_Prel.Codice=dbo.Conferimenti.SottoTipo_Prelievo )
-   INNER JOIN dbo.Conferimenti_Finalita ON ( dbo.Conferimenti.Anno=dbo.Conferimenti_Finalita.Anno and dbo.Conferimenti.Numero=dbo.Conferimenti_Finalita.Numero )
-   INNER JOIN dbo.Anag_Finalita  dbo_Anag_Finalita_Confer ON ( dbo.Conferimenti_Finalita.Finalita=dbo_Anag_Finalita_Confer.Codice )
-   LEFT OUTER JOIN dbo.Anag_Sottotipo_Prel_Dett ON ( dbo.Conferimenti.SottoTipoPrel_Dett=dbo.Anag_Sottotipo_Prel_Dett.Codice )
-   LEFT OUTER JOIN dbo.RDP_Date_Emissione ON ( dbo.RDP_Date_Emissione.Anno=dbo.Conferimenti.Anno and dbo.RDP_Date_Emissione.Numero=dbo.Conferimenti.Numero )
-  }
-WHERE
-( dbo.Laboratori_Reparto.Laboratorio > 1  )
-  AND  dbo.Esami_Aggregati.Esame_Altro_Ente = 0
-  AND  dbo.Esami_Aggregati.Esame_Altro_Ente = 0
-  AND  (
-  {fn year(dbo.Conferimenti.Data)}  =  2022
-  AND  dbo.Anag_Registri.Descrizione  NOT IN  ('Altri Controlli (cosmetici,ambientali..)', 'Controlli Interni Sistema Qualità')
-  )"
+queryTaut <- "SELECT        dbo.Conferimenti.Numero, dbo.Anag_Registri.Descrizione, dbo_Anag_Finalita_Confer.Descrizione AS finalita, dbo.Conferimenti.Data_Prelievo, dbo.Conferimenti.Data AS dtconf, dbo.Conferimenti.Data_Accettazione AS dtreg, 
+                         dbo.Conferimenti.Data_Inserimento AS dtprog, dbo.Esami_Aggregati.Data_Invio, dbo.Esami_Aggregati.Data_Carico, dbo.Esami_Aggregati.Data_Inizio_Analisi, dbo.Esami_Aggregati.Data_Fine_Analisi, 
+                         dbo.Esami_Aggregati.Codice AS nprogrammazione, dbo.Anag_Gruppo_Prove.Descrizione AS categprove, dbo_Anag_Prove_Gruppi.Descrizione AS gruppo, dbo.Anag_Prove.Descrizione AS prova, 
+                         dbo.Anag_Metodi_di_Prova.Descrizione AS mp, dbo_Anag_Reparti_ConfAcc.Descrizione AS repacc, dbo_Anag_Reparti_ConfProp.Descrizione AS repprop, dbo.Anag_Reparti.Descrizione AS repanalisi, 
+                         dbo.Anag_Laboratori.Descrizione AS labanalisi, dbo.Conferimenti.Data_Primo_RDP_Completo_Firmato
+FROM            dbo.Anag_Registri INNER JOIN
+                         dbo.Conferimenti ON dbo.Conferimenti.Registro = dbo.Anag_Registri.Codice LEFT OUTER JOIN
+                         dbo.Esami_Aggregati ON dbo.Conferimenti.Anno = dbo.Esami_Aggregati.Anno_Conferimento AND dbo.Conferimenti.Numero = dbo.Esami_Aggregati.Numero_Conferimento LEFT OUTER JOIN
+                         dbo.Nomenclatore_MP ON dbo.Esami_Aggregati.Nomenclatore = dbo.Nomenclatore_MP.Codice LEFT OUTER JOIN
+                         dbo.Anag_Metodi_di_Prova ON dbo.Nomenclatore_MP.MP = dbo.Anag_Metodi_di_Prova.Codice LEFT OUTER JOIN
+                         dbo.Nomenclatore_Settori ON dbo.Nomenclatore_MP.Nomenclatore_Settore = dbo.Nomenclatore_Settori.Codice LEFT OUTER JOIN
+                         dbo.Nomenclatore ON dbo.Nomenclatore_Settori.Codice_Nomenclatore = dbo.Nomenclatore.Chiave LEFT OUTER JOIN
+                         dbo.Anag_Prove ON dbo.Nomenclatore.Codice_Prova = dbo.Anag_Prove.Codice LEFT OUTER JOIN
+                         dbo.Anag_Gruppo_Prove ON dbo.Nomenclatore.Codice_Gruppo = dbo.Anag_Gruppo_Prove.Codice LEFT OUTER JOIN
+                         dbo.Laboratori_Reparto ON dbo.Esami_Aggregati.RepLab_analisi = dbo.Laboratori_Reparto.Chiave LEFT OUTER JOIN
+                         dbo.Anag_Reparti ON dbo.Laboratori_Reparto.Reparto = dbo.Anag_Reparti.Codice LEFT OUTER JOIN
+                         dbo.Anag_Laboratori ON dbo.Laboratori_Reparto.Laboratorio = dbo.Anag_Laboratori.Codice LEFT OUTER JOIN
+                         dbo.Nomenclatore_MP AS dbo_Nomenclatore_MP_Gruppi ON dbo.Esami_Aggregati.Gruppo_Nomenclatore = dbo_Nomenclatore_MP_Gruppi.Codice LEFT OUTER JOIN
+                         dbo.Nomenclatore_Settori AS dbo_Nomenclatore_Settori_Gruppi ON dbo_Nomenclatore_MP_Gruppi.Nomenclatore_Settore = dbo_Nomenclatore_Settori_Gruppi.Codice LEFT OUTER JOIN
+                         dbo.Nomenclatore AS dbo_Nomenclatore_Gruppi ON dbo_Nomenclatore_Settori_Gruppi.Codice_Nomenclatore = dbo_Nomenclatore_Gruppi.Chiave LEFT OUTER JOIN
+                         dbo.Anag_Prove AS dbo_Anag_Prove_Gruppi ON dbo_Nomenclatore_Gruppi.Codice_Prova = dbo_Anag_Prove_Gruppi.Codice INNER JOIN
+                         dbo.Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfProp ON dbo.Conferimenti.RepLab = dbo_Laboratori_Reparto_ConfProp.Chiave INNER JOIN
+                         dbo.Anag_Reparti AS dbo_Anag_Reparti_ConfProp ON dbo_Laboratori_Reparto_ConfProp.Reparto = dbo_Anag_Reparti_ConfProp.Codice INNER JOIN
+                         dbo.Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfAcc ON dbo.Conferimenti.RepLab_Conferente = dbo_Laboratori_Reparto_ConfAcc.Chiave INNER JOIN
+                         dbo.Anag_Reparti AS dbo_Anag_Reparti_ConfAcc ON dbo_Laboratori_Reparto_ConfAcc.Reparto = dbo_Anag_Reparti_ConfAcc.Codice INNER JOIN
+                         dbo.Conferimenti_Finalita ON dbo.Conferimenti.Anno = dbo.Conferimenti_Finalita.Anno AND dbo.Conferimenti.Numero = dbo.Conferimenti_Finalita.Numero INNER JOIN
+                         dbo.Anag_Finalita AS dbo_Anag_Finalita_Confer ON dbo.Conferimenti_Finalita.Finalita = dbo_Anag_Finalita_Confer.Codice
+WHERE        (dbo.Laboratori_Reparto.Laboratorio > 1) AND (dbo.Esami_Aggregati.Esame_Altro_Ente = 0) AND (dbo.Esami_Aggregati.Esame_Altro_Ente = 0) AND ({ fn YEAR(dbo.Conferimenti.Data_Accettazione) } = 2022) AND 
+                         (dbo.Anag_Registri.Descrizione IN ('Alimenti Uomo', 'Alimenti Zootecnici', 'Sanità Animale'))"
 
 
+
+
+
+
+
+# queryTaut <- "SELECT        
+# dbo.Conferimenti.Data_Prelievo, 
+# dbo.Conferimenti.Data, 
+# dbo_Anag_Reparti_ConfAcc.Descrizione AS stracc, 
+# dbo_Anag_Reparti_ConfProp.Descrizione AS strapp, 
+# dbo.Conferimenti.Numero AS nconf, 
+# dbo_Anag_Finalita_Confer.Descrizione AS Finalita, 
+# DATENAME(weekday, dbo.Conferimenti.Data_Accettazione) AS giorno, 
+# dbo.Conferimenti.Data_Accettazione, dbo.Esami_Aggregati.Codice AS codprogram, 
+# dbo.Anag_Prove.Descrizione AS prova, dbo.Esami_Aggregati.Data_Invio, 
+# dbo.Esami_Aggregati.Data_Carico, dbo.Anag_Reparti.Descrizione AS repprova, 
+# dbo.Anag_Laboratori.Descrizione AS Laboratorio, 
+#                          dbo.Esami_Aggregati.Data_Inizio_Analisi, 
+#                          dbo.Esami_Aggregati.Data_Fine_Analisi, 
+#                          dbo.RDP_Date_Emissione.Data_RDP, 
+#                          dbo.Conferimenti.DataOra_Primo_RDP_Completo_Firmato, 
+#                          dbo.Conferimenti.NrCampioni, 
+#                          dbo.Esami_Aggregati.Tot_Eseguiti, 
+#                          dbo.Nomenclatore.Chiave, 
+#                          dbo.Anag_Metodi_di_Prova_Base.Descrizione AS mmpp, 
+#                          dbo.Anag_Gruppo_Prove.Descrizione AS catprova, 
+#                          dbo.Anag_Registri.Descrizione AS reg, 
+#                          dbo.Anag_Tipo_Prel.Descrizione AS tipoprelievo, 
+#                          dbo.Anag_Sottotipo_Prel.Descrizione AS sottotipoprel, 
+#                          dbo.Anag_Sottotipo_Prel_Dett.Descrizione AS stipopreldesc, 
+#                          dbo.Anag_Comuni.Descrizione AS Comune, 
+#                          dbo.Anag_Comuni.Provincia, 
+#                          dbo.Anag_Regioni.Descrizione AS Regione, 
+#                          dbo.Conferimenti.Data_Inserimento
+# FROM            dbo.Anag_Reparti AS dbo_Anag_Reparti_ConfAcc INNER JOIN
+#                          dbo.Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfAcc ON dbo_Laboratori_Reparto_ConfAcc.Reparto = dbo_Anag_Reparti_ConfAcc.Codice INNER JOIN
+#                          dbo.Conferimenti ON dbo.Conferimenti.RepLab_Conferente = dbo_Laboratori_Reparto_ConfAcc.Chiave INNER JOIN
+#                          dbo.Anag_Comuni ON dbo.Anag_Comuni.Codice = dbo.Conferimenti.Luogo_Prelievo LEFT OUTER JOIN
+#                          dbo.Anag_Regioni ON dbo.Anag_Regioni.Codice = dbo.Anag_Comuni.Regione LEFT OUTER JOIN
+#                          dbo.Esami_Aggregati ON dbo.Conferimenti.Anno = dbo.Esami_Aggregati.Anno_Conferimento AND dbo.Conferimenti.Numero = dbo.Esami_Aggregati.Numero_Conferimento LEFT OUTER JOIN
+#                          dbo.Nomenclatore_MP ON dbo.Esami_Aggregati.Nomenclatore = dbo.Nomenclatore_MP.Codice LEFT OUTER JOIN
+#                          dbo.Nomenclatore_Settori ON dbo.Nomenclatore_MP.Nomenclatore_Settore = dbo.Nomenclatore_Settori.Codice LEFT OUTER JOIN
+#                          dbo.Nomenclatore ON dbo.Nomenclatore_Settori.Codice_Nomenclatore = dbo.Nomenclatore.Chiave LEFT OUTER JOIN
+#                          dbo.Anag_Prove ON dbo.Nomenclatore.Codice_Prova = dbo.Anag_Prove.Codice LEFT OUTER JOIN
+#                          dbo.Anag_Gruppo_Prove ON dbo.Nomenclatore.Codice_Gruppo = dbo.Anag_Gruppo_Prove.Codice LEFT OUTER JOIN
+#                          dbo.Anag_Metodi_di_Prova_Revisioni ON dbo.Nomenclatore_MP.MP = dbo.Anag_Metodi_di_Prova_Revisioni.Codice LEFT OUTER JOIN
+#                          dbo.Anag_Metodi_di_Prova_Base ON dbo.Anag_Metodi_di_Prova_Revisioni.MP_Base = dbo.Anag_Metodi_di_Prova_Base.Codice LEFT OUTER JOIN
+#                          dbo.Laboratori_Reparto ON dbo.Esami_Aggregati.RepLab_analisi = dbo.Laboratori_Reparto.Chiave LEFT OUTER JOIN
+#                          dbo.Anag_Reparti ON dbo.Laboratori_Reparto.Reparto = dbo.Anag_Reparti.Codice LEFT OUTER JOIN
+#                          dbo.Anag_Laboratori ON dbo.Laboratori_Reparto.Laboratorio = dbo.Anag_Laboratori.Codice INNER JOIN
+#                          dbo.Anag_Tipo_Prel ON dbo.Conferimenti.Tipo_Prelievo = dbo.Anag_Tipo_Prel.Codice INNER JOIN
+#                          dbo.Anag_Registri ON dbo.Conferimenti.Registro = dbo.Anag_Registri.Codice INNER JOIN
+#                          dbo.Laboratori_Reparto AS dbo_Laboratori_Reparto_ConfProp ON dbo.Conferimenti.RepLab = dbo_Laboratori_Reparto_ConfProp.Chiave INNER JOIN
+#                          dbo.Anag_Reparti AS dbo_Anag_Reparti_ConfProp ON dbo_Laboratori_Reparto_ConfProp.Reparto = dbo_Anag_Reparti_ConfProp.Codice LEFT OUTER JOIN
+#                          dbo.Anag_Sottotipo_Prel ON dbo.Anag_Sottotipo_Prel.Codice = dbo.Conferimenti.SottoTipo_Prelievo INNER JOIN
+#                          dbo.Conferimenti_Finalita ON dbo.Conferimenti.Anno = dbo.Conferimenti_Finalita.Anno AND dbo.Conferimenti.Numero = dbo.Conferimenti_Finalita.Numero INNER JOIN
+#                          dbo.Anag_Finalita AS dbo_Anag_Finalita_Confer ON dbo.Conferimenti_Finalita.Finalita = dbo_Anag_Finalita_Confer.Codice LEFT OUTER JOIN
+#                          dbo.Anag_Sottotipo_Prel_Dett ON dbo.Conferimenti.SottoTipoPrel_Dett = dbo.Anag_Sottotipo_Prel_Dett.Codice LEFT OUTER JOIN
+#                          dbo.RDP_Date_Emissione ON dbo.RDP_Date_Emissione.Anno = dbo.Conferimenti.Anno AND dbo.RDP_Date_Emissione.Numero = dbo.Conferimenti.Numero
+# WHERE        (dbo.Laboratori_Reparto.Laboratorio > 1) AND (dbo.Esami_Aggregati.Esame_Altro_Ente = 0) AND (dbo.Esami_Aggregati.Esame_Altro_Ente = 0) AND ({ fn YEAR(dbo.Conferimenti.Data) } = 2022) AND 
+#                          (dbo.Anag_Registri.Descrizione NOT IN ('Altri Controlli (cosmetici,ambientali..)', 'Controlli Interni Sistema Qualità')) 
+# "
+# 
+# 
 
 #query con aggiunta della tabella RDP_storico ----
 
